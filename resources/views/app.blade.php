78c9b1d12df215c6200c64bc54fac53e8da3cb1b
anggarasa <anggasaputra6259@gmail.com>
2025-10-16 08:26:09 +0700

    add verifikasi search google console

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,26 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="google-site-verification" content="3FcMQUs5E7ZJfJ1bPthnMgXHx1xB8FZFYD8_orcWpro" />
+
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-38JVC9NS3D"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-38JVC9NS3D');
+        </script>
+
+        <!-- Google Tag Manager -->
+        <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TD8H3JCK');
+        </script>
+        <!-- End Google Tag Manager -->
 
         {{-- Inline script to detect system dark mode preference and apply it immediately --}}
         <script>
@@ -64,6 +84,11 @@
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
+        <!-- Google Tag Manager (noscript) -->
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TD8H3JCK"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        <!-- End Google Tag Manager (noscript) -->
+
         {{-- Google Tag Manager (noscript) --}}
         @if(app(\App\Services\AnalyticsService::class)->isEnabled())
             {!! app(\App\Services\AnalyticsService::class)->renderGTMBody() !!}
